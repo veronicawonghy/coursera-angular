68015c4889e9153c2af6b1aab8ae6b8cf3121a8e
veronicawonghy <veronicawonghy@gmail.com>
2020-08-10 18:06:57 +0800

    assignment 1 updated

--- assignments/module1-solution/app.js
+++ assignments/module1-solution/app.js
@@ -9,17 +9,26 @@
     function LunchCheckController($scope) {
         $scope.dishes = '';
         $scope.message = '';
+        $scope.empty = false;
+        $scope.checked = false;
 
         $scope.checkLunch = function () {
+            $scope.checked = true;
             var dishesArray = $scope.dishes.split(',').filter(function (v) {
                 return v.trim() !== '';
             })
             console.log(dishesArray)
 
-            if (dishesArray.length <= 3) {
+            if(dishesArray.length == 0){
+                $scope.empty = true;
+                $scope.message = 'Please enter data first'
+            }
+            else if (dishesArray.length <= 3) {
+                $scope.empty = false;
                 $scope.message = 'Enjoy!';
             }
             else {
+                $scope.empty = false;
                 $scope.message = 'Too much!';
             }
 
