ead29410c75ccbee9b7e10d8d86a06d283a977e2
veronicawonghy <veronicawonghy@gmail.com>
2020-08-21 14:31:17 +0800

    assignment2 update

--- assignments/module2-solution/app.js
+++ assignments/module2-solution/app.js
@@ -30,10 +30,10 @@
         var service = this;
         var toBuyItems = [
             { name: "cookies", quantity: 10 },
-            { name: "cokes", quantity: 2 },
+            { name: "cakes", quantity: 2 },
             { name: "beers", quantity: 6 },
             { name: "apples", quantity: 4 },
-            { name: "bananas", quantity: 7 }
+            { name: "bananas", quantity: 7 },
         ];
         var alreadyBoughtItems = [];
 
